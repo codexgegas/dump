d0abbdd6781c0afd8a92ff577998cecef9dc17ac
Vikram <51349461+codexgegas@users.noreply.github.com>
2021-11-29 15:31:36 +0530

    Add files via upload

---
+++ Cryptobase/cemail.php
@@ -0,0 +1,49 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Change Email</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<form action="change-E.php" method="post">
+    <h2>Change Email</h2>
+    <?php if (isset($_GET['error'])) { ?>
+     	<p class="error"><?php echo $_GET['error']; ?></p>
+    <?php } ?>
+
+    <label>Old Email</label>
+    <input type="Email" 
+           name="oe" 
+           placeholder="Old Email">
+           <br>
+
+    <label>New Email</label>
+    <input type="Email" 
+           name="ne" 
+           placeholder="New email">
+           <br>
+
+    <label>Confirm New Email</label>
+    <input type="Email" 
+           name="c_ne" 
+           placeholder="Confirm New Email">
+           <br>
+
+    <button type="submit">Change</button>
+        <a href="home.php" class="ca">Home</a>
+     </form>
+</body>
+</html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
--- Cryptobase/display.php
+++ Cryptobase/display.php
@@ -1,6 +1,6 @@
 <?php
-include_once "parts/config.php";
- $sql= "SELECT * FROM `coins`";
+include_once "../CRYPTOBASE/parts/config.php";
+ $sql= "SELECT * FROM `coin_table`";
  $result=mysqli_query($conn, $sql);
  $nums= mysqli_num_rows($result);
 echo "connected"
